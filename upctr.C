5e5f5f24b2eecdf19e486e8d2230020a735e1a5b
Michael Driscoll <mbdriscoll@gmail.com>
2011-10-13 18:17:59 -0700

    initial files for cs294-77 upc translator

---
+++ upctr.C
@@ -0,0 +1,11 @@
+/* upctr.c: A standard "Hello, world!" program */
+ 
+#include <iostream>
+ 
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello, world!\n" << endl;
+    return 0;
+}
